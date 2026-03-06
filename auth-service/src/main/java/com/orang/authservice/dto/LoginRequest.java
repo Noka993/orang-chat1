8fba17a7cf7434307c43a5c8745701e7c155b3f0
Noka993 <Noka993@wp.pl>
2026-03-06 16:09:29 +0100

    Add AuthResponse, LoginRequest and RegisterRequest

--- auth-service/src/main/java/com/orang/authservice/dto/LoginRequest.java
+++ auth-service/src/main/java/com/orang/authservice/dto/LoginRequest.java
@@ -6,18 +6,13 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class LoginRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
-    @Size(min = 5, max = 100, message = "Email must be between 5 and 100 characters long")
+    @Size(min = 5, max = 100)
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
-
-    @NotBlank(message = "Display name is required")
-    @Size(min = 2, max = 50, message = "Display name must be between 2 and 50 characters")
-    private String displayName;
 }
