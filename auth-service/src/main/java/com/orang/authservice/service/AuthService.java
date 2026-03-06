97cb7eacefafd770e911bdafc82d5623fff5840c
Noka993 <Noka993@wp.pl>
2026-03-06 16:38:03 +0100

    Add UserRepository

--- auth-service/src/main/java/com/orang/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/orang/authservice/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.orang.authservice.service;
 
+import com.orang.authservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
