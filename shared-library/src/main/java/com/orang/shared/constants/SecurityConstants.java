110293656be649b0ce675b9698fdc75aa0497619
Noka993 <Noka993@wp.pl>
2026-03-06 18:26:50 +0100

    Undo putting things into shared library

--- shared-library/src/main/java/com/orang/shared/constants/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.orang.shared.constants;
-
-public class SecurityConstants {
-    private static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String TOKEN_TYPE = "Bearer";
-
-    public SecurityConstants() {}
-}
